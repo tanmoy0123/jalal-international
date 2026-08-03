e0ea698e0d9d0f04fc1de57c29cb6b251cad1246
Tanmoy Mozumder <tanmoymozumder0@gmail.com>
2026-08-03 12:16:27 +0600

    cursor effect and bottom to top done

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnDestroy, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './component/global/header/header';
 import { Footer } from './component/global/footer/footer';
@@ -9,6 +9,46 @@ declare var AOS: any;
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements AfterViewInit, OnDestroy {
   protected readonly title = signal('frontend');
+  private mouseMoveListener!: (e: MouseEvent) => void;
+  isShowScrollTopBtn = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const yOffset = window.pageYOffset || document.documentElement.scrollTop;
+    this.isShowScrollTopBtn = yOffset > 300;
+  }
+
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+
+  ngAfterViewInit(): void {
+    const dot = document.getElementById('cursor-dot');
+    const outline = document.getElementById('cursor-outline');
+
+    this.mouseMoveListener = (e: MouseEvent) => {
+      const posX = e.clientX;
+      const posY = e.clientY;
+
+      requestAnimationFrame(() => {
+        if (dot && outline) {
+          dot.style.transform = `translate(calc(${posX}px - 50%), calc(${posY}px - 50%))`;
+          outline.style.transform = `translate(calc(${posX}px - 50%), calc(${posY}px - 50%))`;
+        }
+      });
+    };
+
+    window.addEventListener('mousemove', this.mouseMoveListener);
+  }
+
+  ngOnDestroy(): void {
+    if (this.mouseMoveListener) {
+      window.removeEventListener('mousemove', this.mouseMoveListener);
+    }
+  }
 }
